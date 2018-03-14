f07a74e020101b6fd0ecca06a62991e8654c4ddc
Raquel Calderon <rcalderon@bluebeam.com>
2018-03-13 20:39:20 -0700

    Arrow functions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import Note from "./Note";
-import NewNoteForm from './NewNoteForm';
+import NewNoteForm from "./NewNoteForm";
 import "./App.css";
 
 const defaultNotes = [{ id: 0, text: "cats" }, { id: 1, text: "dogs" }];
 
 class App extends Component {
-  state = { notes: defaultNotes, inputValue: '' };
+  state = { notes: defaultNotes, inputValue: "" };
 
-  changeNewNoteValue(e) {
-    const { notes } = this.state
+  changeNewNoteValue = (e) => {
+    const { notes } = this.state;
     const newNoteValue = e.target.value;
-    this.setState({ notes, inputValue: newNoteValue })
+    this.setState({ notes, inputValue: newNoteValue });
   }
 
-  addNote() {
+  addNote = () => {
     const { notes, inputValue } = this.state;
-    const newNotes = notes.concat([
-      { id: notes.length, text: inputValue }
-    ]);
+    const newNotes = notes.concat([{ id: notes.length, text: inputValue }]);
     this.setState({ notes: newNotes, inputValue });
   }
 
@@ -31,8 +29,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <NewNoteForm onChange={this.changeNewNoteValue.bind(this)} onSubmit={this.addNote.bind(this)}/>
-        <div className="notes-list">{notes.map(note => <Note key={note.id} text={note.text} />)}</div>
+        <NewNoteForm
+          onChange={this.changeNewNoteValue}
+          onSubmit={this.addNote}
+        />
+        <div className="notes-list">
+          {notes.map(note => <Note key={note.id} text={note.text} />)}
+        </div>
       </div>
     );
   }
